f91aff58e7739b9e78af24367fc13cb8d264d5d5
Ayush <dixitayush19d@gmail.com>
2025-07-26 18:10:02 +0530

    21 ko hatake 23 tak done

--- src/Suru.java
+++ src/Suru.java
@@ -224,7 +224,35 @@ public class Suru {
             System.out.println("Invalid value");
         }
         */
-
+    /*21*/
+        /*22*/
+        System.out.println("Enter Alphabet: ");
+        char ch =sc.nextLine().charAt(0);
+        switch(ch){
+            case 'a','A' : {
+                System.out.println("Vowel");
+                break;
+            }
+            case 'e','E' : {
+                System.out.println("Vowel");
+                break;
+            }
+            case 'i','I' : {
+                System.out.println("Vowel");
+                break;
+            }
+            case 'o','O' : {
+                System.out.println("Vowel");
+                break;
+            }
+            case 'u','U' : {
+                System.out.println("Vowel");
+                break;
+            }
+            default:{
+                System.out.println("Consonant");
+            }
+        }
     }
 }
 
