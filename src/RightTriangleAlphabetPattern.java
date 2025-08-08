b97069072daa2f1bf8725bbe24d8f56a6c0a3a63
Ayush <dixitayush19d@gmail.com>
2025-08-08 15:32:44 +0530

    Some more Questions completed of patterns

---
+++ src/RightTriangleAlphabetPattern.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class RightTriangleAlphabetPattern {
+    public static void main(String[]args){
+        char ch = sc.nextLine().charAt(0);
+        for(char ah=65;ah<=ch;ah++){
+            for(char bh =65;bh<=ah;bh++){
+                System.out.print(bh + " ");
+            }
+        }
+        System.out.println();  Scanner sc = new Scanner (System.in);
+
+    }
+}
