ceaf45959d1d9beef1c0adfcdf6ff1f73fff4655
Ayush <dixitayush19d@gmail.com>
2025-08-08 16:39:43 +0530

    Pattern complete except x and v pattern

--- src/RightTriangleAlphabetPattern.java
+++ src/RightTriangleAlphabetPattern.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class RightTriangleAlphabetPattern {
     public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
         char ch = sc.nextLine().charAt(0);
         for(char ah=65;ah<=ch;ah++){
             for(char bh =65;bh<=ah;bh++){
                 System.out.print(bh + " ");
             }
         }
-        System.out.println();  Scanner sc = new Scanner (System.in);
+        System.out.println();
 
     }
 }
