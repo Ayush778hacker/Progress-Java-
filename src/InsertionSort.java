12276f2d3a7d8dcb33b0cca02668f54ce1b8334f
Ayush Dixit <dixitayush19d@gmail.com>
2025-09-05 10:03:50 +0530

    INsetion Sort And SelectionSort

---
+++ src/InsertionSort.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String []args){
+        int[] arr = {8,2,1,5,12,9,27,16};
+        int n= arr.length;
+        for(int i=1;i<n;i++){
+            int key = arr[i];
+            int j= i-1;
+            while(j>=0 && key<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
